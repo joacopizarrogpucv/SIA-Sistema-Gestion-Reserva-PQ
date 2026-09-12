148cc3922d08379ccb3a9aac7e398792f0452b25
joacopizarrogpucv <joaquin.pizarro.g@mail.pucv.cl>
2026-09-11 22:41:27 -0300

    Actualización de Cabaña y Camping

--- Cabana.java
+++ Cabana.java
@@ -1,10 +1,21 @@
-public class Cabana extends Recurso {
+import java.util.Map;
+import java.util.HashMap;
+import java.util.ArrayList;
 
+public class Cabana extends Recurso {
     private int cantidadHabitaciones;
+
+    // mes -> dias ocupados
+    private Map<Integer, ArrayList<Integer>> diasOcupados;
 
     public Cabana(String n, int c, int habitaciones) {
         super(n, c);
         cantidadHabitaciones = habitaciones;
+        diasOcupados = new HashMap<Integer, ArrayList<Integer>>();
+
+        for (int mes = 1; mes <= 12; mes++) {
+            diasOcupados.put(mes, new ArrayList<Integer>());
+        }
     }
 
     public int getCantidadHabitaciones() {
@@ -13,5 +24,25 @@ public class Cabana extends Recurso {
 
     public void setCantidadHabitaciones(int habitaciones) {
         cantidadHabitaciones = habitaciones;
+    }
+
+    public boolean diaOcupado(int mes, int dia) {
+        if (!diasOcupados.containsKey(mes)) {
+            return true;
+        }
+
+        return diasOcupados.get(mes).contains(dia);
+    }
+
+    public void ocuparDia(int mes, int dia) {
+        if (!diaOcupado(mes, dia)) {
+            diasOcupados.get(mes).add(dia);
+        }
+    }
+
+    public void liberarDia(int mes, int dia) {
+        if (diasOcupados.containsKey(mes)) {
+            diasOcupados.get(mes).remove(Integer.valueOf(dia));
+        }
     }
 }
